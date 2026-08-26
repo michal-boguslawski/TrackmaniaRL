Add done masking. Fix preprocessing observation to not inplace

scripts/train.py
@@ -82,7 +82,7 @@ def main():
 
     rng = np.random.default_rng(seed=42)
 
-    training_steps = 3_000_000
+    training_steps = 1_000_000
     rollout_collector = RolloutCollector(
         env,
         buffer,

src/rl_lib/agent.py
@@ -34,9 +34,7 @@ class Agent:
 
     def _preprocess_observation(self, observation: T.Tensor) -> T.Tensor:
         """Input shape (batch, height, width, channel)"""
-        observation_tensor = observation.to(self._device, T.float32)
-        observation_tensor.div_(255. / 2.).sub_(1.)
-
+        observation_tensor = observation.to(self._device, T.float32) / (255. / 2.) - 1.
         return observation_tensor.permute(0, 3, 1, 2)
 
     def feature_extract(self, observation: T.Tensor) -> T.Tensor:
@@ -81,14 +79,14 @@ class Agent:
 
             temporal = self.temporal_encode(
                 self._get_features_window(features),
-                # self._get_mask_window(done),
+                self._get_mask_window(done),
             )
 
             action_dist, value = self.heads(temporal, temperature)
 
             action = action_dist.sample()
-            action_tensor = T.clamp(action, self._clamp_min + 1e-6, self._clamp_max - 1e-6)
-            log_probs = action_dist.log_prob(action_tensor).sum(dim=-1)
+            action = T.clamp(action, self._clamp_min + 1e-6, self._clamp_max - 1e-6)
+            log_probs = action_dist.log_prob(action).sum(dim=-1)
 
         return (
             action,
@@ -99,9 +97,17 @@ class Agent:
     def evaluate_actions(
         self, observation: T.Tensor | T.Tensor, action: T.Tensor, dones: T.Tensor | None = None
     ) -> tuple[T.Tensor, T.Tensor, Distribution]:
+        print(
+            observation.dtype,
+            observation.min().item(),
+            observation.max().item(),
+        )
         extracted_features = self.feature_extract(observation)
         windowed_features = extracted_features.unfold(0, self._stack_size, self._stack_size).permute(0, 2, 1)
-        temporal_encoding = self.temporal_encode(windowed_features, None)
+        
+        windowed_dones = dones.unfold(0, self._stack_size, self._stack_size)  # (batch, stack_size)
+        mask = windowed_dones.logical_not() & (windowed_dones.flip(1).cumsum(1).flip(1) > 0)
+        temporal_encoding = self.temporal_encode(windowed_features, mask)
         action_dist, values = self.heads(temporal_encoding)
 
         action_tensor = T.clamp(action, self._clamp_min + 1e-6, self._clamp_max - 1e-6)

src/rl_lib/networks/factory.py
@@ -5,6 +5,7 @@ from torch.distributions import Distribution
 from rl_lib.networks.actor import Actor
 from rl_lib.networks.critic import Critic
 from rl_lib.networks.cnn import CNN
+from rl_lib.networks.temporal import TemporalCNN1D
 
 
 class Network(nn.Module):
@@ -12,9 +13,7 @@ class Network(nn.Module):
         super().__init__()
 
         self.cnn = CNN(observation_dim)
-        self.sequence_encoder = nn.Conv1d(
-            256, 256, kernel_size=stack_size
-        )
+        self.sequence_encoder = TemporalCNN1D(stack_size)
         self.norm = nn.LayerNorm(256)
         self.actor = Actor(action_dim)
         self.critic = Critic()
@@ -25,12 +24,7 @@ class Network(nn.Module):
 
     def temporal_encode(self, x: T.Tensor) -> T.Tensor:
         """Expects input of shape (batch, seq, feature)"""
-        x = x.permute(0, 2, 1)
-        x = self.sequence_encoder(x)
-        x = x.permute(0, 2, 1)
-        x.squeeze_(1)
-        x = self.norm(x)
-        return x
+        return self.sequence_encoder(x)
 
     def heads(self, x: T.Tensor, temperature: float) -> tuple[Distribution, T.Tensor]:
         """Expects input of shape (batch, feature)"""
@@ -56,4 +50,4 @@ class Network(nn.Module):
         T.save(self.state_dict(), path)
 
     def load_state_dict(self, path: str) -> None:
-        self.load_state_dict(T.load(path, weights_only=True))
+        super().load_state_dict(T.load(path, weights_only=True))